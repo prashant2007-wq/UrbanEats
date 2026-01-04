ed8db7e5a096be8d3768c719ac7be017e2495955
PRASHANT S BISHT <prashantjee2025@gmail.com>
2026-01-04 23:42:17 +0530

    Fix formatting of event listener in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() 
+{
   const button = document.querySelector("#toggleBtn");
   const toggleText = document.querySelector(".toggle-text");
   const body = document.querySelector("body");
